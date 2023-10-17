7ba765e34401f3089aa80aafa1daaf3783d46d2d
alissonff <alisson.franco@gmail.com>
2023-10-16 22:21:14 -0400

    prettier

--- scraper.js
+++ scraper.js
@@ -1,70 +1,90 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function scrapeDepartment(department) {
-    const browser = await puppeteer.launch({
-        headless: "new"
-    });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
+  const page = await browser.newPage();
 
-    // Navigate to the Amazon.ca department deals page
-    await page.goto(`url`);
+  // Navigate to the Amazon.ca department deals page
+  await page.goto(`url`);
 
-    // Add scraping logic for the department here if needed
+  // Add scraping logic for the department here if needed
 
-    await browser.close();
-    // Return the scraped data for the department here
+  await browser.close();
+  // Return the scraped data for the department here
 }
 
 async function scrapeProduct(url) {
-    console.log("Scraping function called3");
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  console.log("Scraping function called3");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-    await page.goto(url);
+  await page.goto(url);
 
-    //await page.waitForSelector('.a-price-whole', 'a-price a-text-price header-price a-size-base a-text-normal');
-    const priceSelectors = ['.a-price-whole', 'a-price a-text-price header-price a-size-base a-text-normal'];
-    const founSelector = await waitForMultipleSelectors(page, priceSelectors);
+  //await page.waitForSelector('.a-price-whole', 'a-price a-text-price header-price a-size-base a-text-normal');
+  const priceSelectors = [
+    ".a-price-whole",
+    "a-price a-text-price header-price a-size-base a-text-normal",
+  ];
+  const founSelector = await waitForMultipleSelectors(page, priceSelectors);
 
-    console.log("Scraping function called");
+  console.log("Scraping function called");
 
-    const productDetails = await page.evaluate((founSelector) => {
-        console.log("Scraping function called2");
-        let title = document.getElementById('productTitle')?.textContent?.trim();
+  const productDetails = await page.evaluate((founSelector) => {
+    console.log("Scraping function called2");
+    let title = document.getElementById("productTitle")?.textContent?.trim();
 
-        let price;
+    let price;
 
-        if (founSelector === '.a-price-whole'){
-            let priceWholeElement = document.getElementsByClassName('a-price-whole');
-            let priceFractionElement = document.getElementsByClassName('a-price-fraction');
-            let priceWhole = priceWholeElement.length > 0 ? priceWholeElement[0].textContent.trim() : null;
-            let priceFraction = priceFractionElement.length > 0 ? priceFractionElement[0].textContent.trim() : null;
-            // Combine the whole and fraction parts of the price
-            price = priceWhole && priceFraction ? `${priceWhole}${priceFraction}` : priceWhole;
-        } else if (founSelector === 'a-price a-text-price header-price a-size-base a-text-normal') {
-            let priceOneTime = priceOneTimeElement.length > 0 ? priceOneTimeElement[0].textContent.trim() : null;
+    if (founSelector === ".a-price-whole") {
+      let priceWholeElement = document.getElementsByClassName("a-price-whole");
+      let priceFractionElement =
+        document.getElementsByClassName("a-price-fraction");
+      let priceWhole =
+        priceWholeElement.length > 0
+          ? priceWholeElement[0].textContent.trim()
+          : null;
+      let priceFraction =
+        priceFractionElement.length > 0
+          ? priceFractionElement[0].textContent.trim()
+          : null;
+      // Combine the whole and fraction parts of the price
+      price =
+        priceWhole && priceFraction
+          ? `${priceWhole}${priceFraction}`
+          : priceWhole;
+    } else if (
+      founSelector ===
+      "a-price a-text-price header-price a-size-base a-text-normal"
+    ) {
+      let priceOneTime =
+        priceOneTimeElement.length > 0
+          ? priceOneTimeElement[0].textContent.trim()
+          : null;
+    }
 
-        }
-    
-        let rating = document.querySelector('.a-icon-alt')?.textContent?.trim();
-        let numberOfReviews = document.getElementById('acrCustomerReviewText')?.textContent?.trim();
-    
-        return {
-            title,
-            price,
-            priceOneTime,
-            rating,
-            numberOfReviews
-        };
-    }, founSelector);
+    let rating = document.querySelector(".a-icon-alt")?.textContent?.trim();
+    let numberOfReviews = document
+      .getElementById("acrCustomerReviewText")
+      ?.textContent?.trim();
 
-    await browser.close();
-    return productDetails;
+    return {
+      title,
+      price,
+      priceOneTime,
+      rating,
+      numberOfReviews,
+    };
+  }, founSelector);
+
+  await browser.close();
+  return productDetails;
 }
 
 module.exports = {
-    scrapeDepartment,
-    scrapeProduct
+  scrapeDepartment,
+  scrapeProduct,
 };
